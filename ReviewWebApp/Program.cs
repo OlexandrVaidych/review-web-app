5630e9a761df66fa94b9af53a6bc47355cb4c868
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2022-04-03 11:35:50 +0300

    Add dependency injection

--- ReviewWebApp/Program.cs
+++ ReviewWebApp/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using ReviewWebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ReviewWebAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ReviewWebAppContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
