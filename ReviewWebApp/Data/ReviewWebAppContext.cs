0bc088e1e65c67a980ffcb97d3cd9629c4ef17cf
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2022-04-03 08:54:02 +0300

    Add database context

---
+++ ReviewWebApp/Data/ReviewWebAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReviewWebApp.Data
+{
+    public class ReviewWebAppContext : DbContext
+    {
+        public ReviewWebAppContext(DbContextOptions<ReviewWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ReviewWebApp.Models.Review> Review { get; set; }
+    }
+}
