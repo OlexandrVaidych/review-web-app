fab0a92d554ad41f2ec7fc44fabbac27e6808227
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2022-05-06 16:48:25 +0300

    Save data to db

--- ReviewWebApp/Controllers/UsersController.cs
+++ ReviewWebApp/Controllers/UsersController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ReviewWebApp.Data;
+using ReviewWebApp.Models;
 
 namespace ReviewWebApp.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly ReviewWebAppContext _context;
+
+        public UsersController(ReviewWebAppContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -11,6 +20,14 @@ namespace ReviewWebApp.Controllers
 
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([Bind("Id, Name")] User user)
+        {
+            _context.Add(user);
+            await _context.SaveChangesAsync();
             return View();
         }
     }
